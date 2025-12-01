3a692c02b9de4989c9c161271c25bf4a6d2288ac
Anyelo Reyes Guevara <aerguevara@gmail.com>
2025-12-01 10:26:41 +0100

    Initial Commit

---
+++ adm-app/adm_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  adm_appApp.swift
+//  adm-app
+//
+//  Created by Anyelo Reyes on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct adm_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
